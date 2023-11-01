fd9668ecc0081562384a709260fecc570348a91b
Benny0906 <bh2896@columbia.edu>
2023-11-01 15:51:42 -0400

    Added pytest dependency and incorporated tests for is_prime function

--- tests/test_prime_bh2896.py
+++ tests/test_prime_bh2896.py
@@ -1,1 +1,9 @@
-from prime_bh2896 import prime_bh2896
+from prime_bh2896.prime_bh2896 import is_prime
+
+def test_is_prime():
+    assert is_prime(2)
+    assert not is_prime(1)
+    assert not is_prime(0)
+    assert is_prime(5)
+    assert not is_prime(9)
+
